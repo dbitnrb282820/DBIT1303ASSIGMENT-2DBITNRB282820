25dcd81f29e6f07a6d067bac6ef6c09082d367a4
dbitnrb282820 <75674325+dbitnrb282820@users.noreply.github.com>
2020-12-08 14:56:11 +0300

    Add files via upload

---
+++ dbitnrb282820 .py
@@ -0,0 +1,48 @@
+# This is a resturant ordering script
+
+
+
+client_name=input("clients_details")
+table=input("table_number")
+order_number=input("order_number")
+print("hi " +client_name+ table + order_number)
+
+#menu list
+
+print("menu list ")
+print ("1.chicken_fry +rice")
+print( "2 beef_stew + ugali")
+print ("3 fufu +goat_stew")
+print ("4 ugali+ fish")
+print ("5 pilau")
+print ("6 mukimo + stew")
+print ("7 matoke_nyama")
+print ("8 grilled_chicken")
+print ("9 wali_wanazi")
+
+input("enter food number:")
+
+
+waiter_order=1-9
+while waiter_order <=1-9:
+    print ("your order has been noted.")
+    waiter_order+=1
+    print ("order complete")
+
+def order():
+        print ("hello you have ordered" + food)
+
+#payment
+
+secret_code="fall"
+payment_code=""
+not_paid =false
+
+while payment_code!=payment_code and not("not_paid"):
+    if payment_code< limit:
+        payment_code=input("enter payment_code")
+        payment_code +=1
+        print("not paid ,please play")
+    else:print("you have paid")
+
+    import script
